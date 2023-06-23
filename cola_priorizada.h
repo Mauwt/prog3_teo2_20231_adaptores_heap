6ce7b8b78343ee78bbc3ab5c5073c3fb6762ef10
rrivas <rrivas@utec.edu.pe>
2023-06-22 21:40:45 -0500

    Update priority queue

--- cola_priorizada.h
+++ cola_priorizada.h
@@ -6,41 +6,106 @@
 #define PROG3_TEO2_20231_ADAPTORES_HEAP_COLA_PRIORIZADA_H
 
 #include <vector>
+#include <functional>
 using namespace std;
 
+namespace utec {
 // Inicialmente vamos a crear un max heap
-template <typename T, typename Container=vector<T>>
-class cola_priorizada {
-    Container cnt;
-    size_t get_parent_index(size_t index) { return index / parent; }
-    size_t get_left_index(size_t parent_index) { return parent_index * 2; }
-    size_t get_right_index(size_t parent_index) { return parent_index * 2 + 1; }
-    bool is_root(size_t index) { return index == 1; }
-    bool has_children(size_t parent_index) {
-        return get_left_index(parent_index) < size(cnt); }
-    bool has_right_child(size_t parent_index) {
-        return get_right_index(parent_index) < size(cnt); }
+    template <
+            typename T,
+            typename Container=vector<T>,
+            typename Compare=less<T>>
+    class priority_queue {
+        Container cnt;
+        size_t get_parent_index(size_t index) { return index / 2; }
+        size_t get_left_index(size_t parent_index) { return parent_index * 2; }
+        size_t get_right_index(size_t parent_index) { return parent_index * 2 + 1; }
+        bool is_root(size_t index) { return index == 1; }
+        bool has_children(size_t parent_index) {
+            return get_left_index(parent_index) < cnt.size(); }
+        bool has_right_child(size_t parent_index) {
+            return get_right_index(parent_index) < cnt.size(); }
 
-    void percolate_up(size_t index) {
-        // 1. Condicion base
-        // 1.1 root
-        if (is_root(index) == true) return;
-        // 1.2 Obtener el indice del padre
-        auto parent_index = get_parent_index(index);
-        // 1.3 Comparar el padre con el hijo
-        if (cnt[index] < cnt[parent_index]) return;
-        // 2. Condicion recursiva
-        // 2.1 Realizo el swap
-        swap(cnt[index], cnt[parent_index]);
-        // 2.2 llamar percolate up (parent index)
-        percolate_up(parent_index);
-    }
-public:
-    cola_priorizada(): cnt(1) {}
+        size_t get_selected_index(size_t parent_index) {
+            auto left_index = get_left_index(parent_index);
+            if (has_right_child(parent_index)) {
+                auto right_index = get_right_index(parent_index);
+                return cnt[left_index] > cnt[right_index]?
+                    left_index:
+                    right_index;
+            }
+            return left_index;
+        }
+
+        void percolate_up(size_t index) {
+            // 1. Condicion base
+            // 1.1 root
+            if (is_root(index) == true) return;
+            // 1.2 Obtener el indice del padre
+            auto parent_index = get_parent_index(index);
+            // 1.3 Comparar el padre con el hijo
+            if (cnt[index] < cnt[parent_index]) return;
+            // 2. Condicion recursiva
+            // 2.1 Realizo el swap
+            swap(cnt[index], cnt[parent_index]);
+            // 2.2 llamar percolate up (parent index)
+            percolate_up(parent_index);
+        }
+
+        void percolate_down(size_t index) {
+            // Condiciones basicas
+            if (!has_children(index)) return;
+            auto child_index = get_selected_index(index);
+            if (cnt[child_index] < cnt[index]) return;
+            // Condiciones recursivas
+            swap(cnt[child_index], cnt[index]);
+            percolate_down(child_index);
+        }
+
+        void heapify(size_t index) {
+            // Obtener el indice del padre
+            auto current_index = get_parent_index(index);
+            while (current_index >= 1) {
+                percolate_down(current_index);
+                --current_index;
+            }
+        }
+    public:
+        priority_queue(): cnt(1) {}
+        explicit priority_queue(const Container& container)
+                : cnt(1 + container.size()) {
+            copy(begin(container),
+                 end(container),
+                 next(begin(cnt)));     // O(n)
+            heapify(size());              // O(n)
+        }
+        size_t size() { return cnt.size() - 1; }
+        bool empty() { return size() == 0; }
+        T top() { return cnt[1]; }
+
+        void push(T value) {
+            cnt.push_back(value);
+            percolate_up(size()); // Mantenerlo como heap
+        }
+
+        template <typename ...Params>
+        void emplace(Params ...params) {
+            cnt.emplace_back(params...);
+            percolate_up(size()); // Mantenerlo como heap
+        }
+
+        void pop() {
+            // Intercambio entre el ultimo valor y el primer valor
+            swap(cnt[1], cnt[size()]);
+            // Elimino el ultimo
+            cnt.pop_back();
+            percolate_down(1);
+        }
 
 
 
 
-};
+    };
+}
 
 #endif //PROG3_TEO2_20231_ADAPTORES_HEAP_COLA_PRIORIZADA_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include "cola_priorizada.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    utec::priority_queue<int> pq1;
+    pq1.push(1);
+    pq1.emplace(20);
+    pq1.emplace(50);
+    pq1.push(8);
+    pq1.push(60);
+    pq1.emplace(70);
+    pq1.emplace(3);
+    while(!pq1.empty()) {
+        cout << pq1.top() << " ";
+        pq1.pop();
+    }
+    cout << endl;
+
+    vector v = {1, 20, 50, 8, 60, 70, 3};
+    utec::priority_queue<int, vector<int>, greater<>> pq2(v);
+    while(!pq2.empty()) {
+        cout << pq2.top() << " ";
+        pq2.pop();
+    }
+    cout << endl;
     return 0;
 }
